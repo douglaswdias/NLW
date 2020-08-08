6550dbe69f26fd298a11a3f4e890b17a1889b092
Douglas Willian Dias <douglas.wdias@hotmail.com>
2020-08-08 19:07:10 -0300

    TeacherList done

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
-import whatsappIcon from '../../Assets/images/icons/whatsapp.svg'
+import TeacherItem from '../../components/TeacherItem'
 
 import './styles.css'
 
@@ -29,34 +28,9 @@ function TeacherList(){
             </PageHeader>
 
             <main>
-                <article className="teacher-item">
-                    <header>
-                        <img src="https://avatars2.githubusercontent.com/u/54633359?s=460&u=4a33fba451e7f846471681cc48e44e70ed3301ff&v=4" alt="Douglas Dias"/>
-                        <div>
-                            <strong>Douglas Dias</strong>
-                            <span>Matemática</span>
-                        </div>
-                    </header>
-
-                    <p>
-                        Na vida não existe uma forma correta, ou a melhor, sempre haverá formas diferentes de se fazer a mesma coisa!
-                        <br/><br/>
-                        3+1 = 4<br/>
-                        2+2 = 4<br/>
-                        15*7 - 1 = 4<br/>
-                    </p>
-
-                    <footer>
-                        <p>
-                            Preço/Hora
-                            <strong>R$ 20,00</strong>
-                        </p>
-                        <button type= "button">
-                            <img src={whatsappIcon} alt="WhatsApp"/>
-                            Entre em Contato
-                        </button>
-                    </footer>
-                </article>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
             </main>
         </div>
     )
